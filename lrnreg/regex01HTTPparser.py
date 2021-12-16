fe80084d59d1ec35df4b419bd658e3dab70d2f6c
Jesse Schaal <jesse.schaal@mutualofomaha.com>
2021-12-16 21:48:28 +0000

    Regex searching with python

---
+++ lrnreg/regex01HTTPparser.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import re
+import urllib.request
+
+def main():
+    """Search a website's content"""
+
+    print("Where should we search")
+    url = input("> ")
+
+    print(f"Great! So we'll try to open this URL {url} to search for the phrase:")
+    searchFor = input("> ")
+
+        # ALL of this is required to strip the text off the response made to the website...
+    # - perform the lookup (urllib.request.urlopen(url)
+    # - then read the attachment (.read())
+    # - finally, translate it from "bytes" to "utf-8" (.decode("utf-8"))
+    #
+    # This is why the requests library was developed!
+
+    searchMe = urllib.request.urlopen(url).read().decode("utf-8")
+
+    # use the re.search() to determine if our website has the pattern we are looking for
+    # it works by taking arguments, the first is the regex search pattern, and the second
+    # is the string to search within
+
+    if re.search(searchFor, searchMe):
+        print("found a match!")
+
+    else:
+        print("No match!")
+
+
+if __name__ == "__main__":
+    main()
